e4dc691f91f18ca9af82811a99b79dced58d69ef
Manuel Steinberg <github@manuel-steinberg.de>
2021-01-28 11:57:33 +0100

    add create stripe customer hook

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php
-@include_once __DIR__ . '/lib/stripe-php-7.69.0/init.php';
+@include_once __DIR__ . '/lib/stripe/init.php';
 
 Kirby::plugin('kreativ-anders/stripekit', [
   'options' => [
-    'sKey' => 'sk_test_xxx'
+    'privateKey' => 'sk_test_xxx'
   ],
   'hooks' => [
     'user.create:after' => function ($user) {
-      $stripe = new \Stripe\StripeClient(
-        'sk_test_66EkZ3GZowFWgxrXszrRnSd200EmvYltOK'
-      );
-      $stripe->customers->create([
-        'description' => $user->email(),
-      ]);
+      // CREATE STRIPE USER
+      try {
+
+        $stripe = new \Stripe\StripeClient(option('kreativ-anders.stripekit.privateKey'));
+        $customer = $stripe->customers->create([
+          'email' => $user->email()
+        ]);
+
+        // UPDATE KIRBY USER
+        $user->update([
+          'stripe_customer' => $customer->id
+        ]);
+
+      } catch(Exception $e) {
+      
+        // LOG ERROR SOMEWHERE !!!
+      }
     }
-  ]
+]
 ]);
 
 
