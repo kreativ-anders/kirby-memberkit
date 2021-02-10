7f8f398cead6de6197ee90975c044f8c57bdaf36
Manuel Steinberg <github@manuel-steinberg.de>
2021-02-10 11:04:39 +0100

    remove comment

--- snippets/stripe-checkout-button.php
+++ snippets/stripe-checkout-button.php
@@ -21,9 +21,7 @@ if($kirby->user()->stripe_subscription()->isEmpty()): ?>
           return stripe.redirectToCheckout({ sessionId: session.id });
         })
         .then(function (result) {
-          // If redirectToCheckout fails due to a browser or network
-          // error, you should display the localized error message to your
-          // customer using error.message.
+
           if (result.error) {
             alert(result.error.message);
           }
