26448a61be7ca0fb5aadd8d7782bab08e00dc702
Manuel Steinberg <github@manuel-steinberg.de>
2021-03-04 13:16:53 +0100

    add siteMethoods for stripe webhook

--- index.php
+++ index.php
@@ -10,6 +10,58 @@ Kirby::plugin('kreativ-anders/memberkit', [
   'hooks'       => include __DIR__ . '/hooks.php',
   'routes'      => include __DIR__ . '/routes.php',
   'userMethods' => include __DIR__ . '/userMethods.php',
+  'siteMethods' => [
+    'updateStripeSubscriptionWebhook' => function ($subscription) {
+
+      $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
+
+      $customer = $stripe->customers->retrieve(
+        $subscription->customer,
+        []
+      );
+
+      $price = $subscription->items['data'][0]->price->id;
+      $priceIndex = array_search($price, array_column(option('kreativ-anders.memberkit.tiers'), 'price'), false);
+
+      $tier = option('kreativ-anders.memberkit.tiers')[$priceIndex]['name'];
+
+      $kirby = kirby();
+      $kirby->impersonate('kirby');
+
+      kirby()->user($customer->email)->update([
+        'stripe_subscription' => $subscription->id,
+        'stripe_status' => $subscription->status,
+        'tier' => $tier
+      ]);
+
+      $kirby->impersonate();    
+    },
+    'cancelStripeSubscriptionWebhook' => function ($subscription) {
+
+      $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
+
+      $customer = $stripe->customers->retrieve(
+        $subscription->customer,
+        []
+      );
+
+      $price = $subscription->items['data'][0]->price->id;
+      $priceIndex = array_search($price, array_column(option('kreativ-anders.memberkit.tiers'), 'price'), false);
+
+      $tier = option('kreativ-anders.memberkit.tiers')[$priceIndex]['name'];
+
+      $kirby = kirby();
+      $kirby->impersonate('kirby');
+
+      kirby()->user($customer->email)->update([
+        'stripe_subscription' => null,
+        'stripe_status' => null,
+        'tier' => option('kreativ-anders.memberkit.tiers')[0]['name']
+      ]);
+
+      $kirby->impersonate();    
+    }
+]
 
 ]);
 
