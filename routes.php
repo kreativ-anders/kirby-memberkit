67f0b78c7f30d82dd33cdf6337a91026983f1871
Manuel Steinberg <github@manuel-steinberg.de>
2021-03-05 14:07:57 +0100

    clean up

--- routes.php
+++ routes.php
@@ -4,32 +4,33 @@
   ROUTES
   ----
   https://getkirby.com/docs/reference/plugins/extensions/routes
-
 */
    
 return function ($kirby) {
   return [
     
-    // CREATE STRIPE CHECKOUT SESSION
+    // CREATE STRIPE CHECKOUT SESSION --------------------------------------------------------------------------------------------------
     [
-      // PATTERN --> STRIPE SLUG / ACTION NAME (SUBSCRIBE) / STRIPE TIER NAME (RAWURLENCODED)
+      // PATTERN => STRIPE SLUG / ACTION NAME (SUBSCRIBE) / STRIPE TIER NAME (RAWURLENCODED)
       'pattern' => Str::lower(option('kreativ-anders.memberkit.stripeURLSlug')) . '/subscribe/(:all)',
       'action' => function ($tier) {
 
+        // DETERMINE PRICE BY TIER NAME
         $tier = rawurldecode($tier);
         $tierIndex = array_search($tier, array_map("Str::lower", array_column(option('kreativ-anders.memberkit.tiers'), 'name')), false);
         $price = option('kreativ-anders.memberkit.tiers')[$tierIndex]['price'];
 
+        // BUILD MAN-IN-THE-MIDDLE/SUCCESS URL => SITE URL / STRIPE SLUG / ACTION NAME (SUCCESS)
         $successURL  = kirby()->site()->url() . '/';
         $successURL .= Str::lower(option('kreativ-anders.memberkit.stripeURLSlug'));
         $successURL .= '/success';
 
         $customer = kirby()->user()->stripe_customer();
+        $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
 
         try {
 
-          // STRIPE CHECKOUT SESSION
-          $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
+          // CREATE STRIPE CHECKOUT SESSION
           $checkout = $stripe->checkout->sessions->create([
             'success_url' => $successURL,
             'cancel_url' => option('kreativ-anders.memberkit.cancelURL'),
@@ -48,6 +49,7 @@ return function ($kirby) {
         } catch(Exception $e) {
         
           // LOG ERROR SOMEWHERE !!!
+          throw new Exception('Could not create stripe checkout session!');
         }     
 
         return [
@@ -55,57 +57,65 @@ return function ($kirby) {
         ];
       }
     ],
-    // CREATE STRIPE CUSTOMER PORTAL SESSION
+    // CREATE STRIPE CUSTOMER PORTAL SESSION ----------------------------------------------------------------------------------------
     [
-      // PATTERN --> STRIPE SLUG / STRIPE PORTAL
+      // PATTERN => STRIPE SLUG / STRIPE PORTAL
       'pattern' => Str::lower(option('kreativ-anders.memberkit.stripeURLSlug')) . '/portal',
       'action' => function () {
 
-        $customer = kirby()->user()->stripe_customer();
-
+        // BUILD MAN-IN-THE-MIDDLE/RETURN URL => SITE URL
         $returnURL  = kirby()->site()->url() . '/';
-        // $returnURL .= Str::lower(option('kreativ-anders.memberkit.stripeURLSlug'));
-        // $returnURL .= '/portal/update';
+        
+        $customer = kirby()->user()->stripe_customer();
+        $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
 
         try {
 
-          // STRIPE PORTAL SESSION
-          $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
+          // CREATE STRIPE PORTAL SESSION
           $session = $stripe->billingPortal->sessions->create([
             'customer' => $customer,
             'return_url' => $returnURL,
           ]);
 
-          $url = $session->url;
-          
+          $url = $session->url; 
       
         } catch(Exception $e) {
         
           // LOG ERROR SOMEWHERE !!!
+          throw new Exception('Could not create stripe portal session!');
         }     
 
+        // GO TO STRIPE PORTAL
         return go($url);
       }
     ],
-    // CANCEL STRIPE SUBSCRIPTION
+    // CANCEL STRIPE SUBSCRIPTION -------------------------------------------------------------------------------------------------
     [
-      // PATTERN --> STRIPE SLUG / ACTION NAME (CANCEL) / TYPE NAME (SUBSCRIPTION)
+      // PATTERN => STRIPE SLUG / ACTION NAME (CANCEL) / TYPE NAME (SUBSCRIPTION)
       'pattern' => Str::lower(option('kreativ-anders.memberkit.stripeURLSlug')) . '/cancel/subscription',
       'action' => function () {
 
         $subscription = kirby()->user()->stripe_subscription();
         $email = kirby()->user()->email();
+        
+        $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
 
         try {
 
           // CANCEL STRIPE SUBSCRIPTION
-          $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
           $stripe->subscriptions->cancel(
             $subscription,
             []
           );
+        } catch(Exception $e) {
+        
+          // LOG ERROR SOMEWHERE !!!
+          throw new Exception('Could not cancel stripe subscription!');
+        } 
 
-          // SUBSCRIPTION STATUS WILL BE "CANCELED"
+        try {
+
+          // RESET KIRBY USER SUBSCRIPTION - ROOT TIER (INDEX=0)
           kirby()->user($email)->update([
             'stripe_subscription' => null,
             'stripe_status' => null,
@@ -115,108 +125,113 @@ return function ($kirby) {
         } catch(Exception $e) {
         
           // LOG ERROR SOMEWHERE !!!
+          throw new Exception('Could not reset kirby user subscriptions!');
         }     
 
         return go();
       }
     ],
-    // UPDATE/MERGE USER AFTER (SUCCESSFUL) CHECKOUT
+    // UPDATE/MERGE KIRBY USER AFTER (SUCCESSFUL) CHECKOUT --------------------------------------------------------------------------
     [
-      // PATTERN --> STRIPE SLUG / success
+      // PATTERN => STRIPE SLUG / ACTION NAME (SUCCESS)
       'pattern' => Str::lower(option('kreativ-anders.memberkit.stripeURLSlug')) . '/success',
       'action' => function () {
 
         try {
 
+          // MERGE STRIPE USER WITH KIRBY USER
           kirby()->user()->mergeStripeCustomer();
 
         } catch(Exception $e) {
         
           // LOG ERROR SOMEWHERE !!!
+          throw new Exception('Could not merge stripe customer into kirby user!');
         }       
 
+        // REDIRECT TO CUSTOM SUCCESS PAGE
         return go(option('kreativ-anders.memberkit.successURL'));
       }
     ],
-    // STRIPE WEBHOOK - OUH YEAH BABY!
+    // LISTEN TO STRIPE NOTIFICATIONS AKA STRIPE WEBHOOK ----------------------------------------------------------------------------
     // https://stripe.com/docs/webhooks/integration-builder
     // --> NOT SECURED!!!
     [
-      // PATTERN --> STRIPE SLUG / ACTION NAME (update)
+      // PATTERN => STRIPE SLUG / ACTION NAME (WEBHOOK)
       'pattern' => Str::lower(option('kreativ-anders.memberkit.stripeURLSlug')) . '/webhook',
       'action' => function () {
 
         \Stripe\Stripe::setApiKey(option('kreativ-anders.memberkit.secretKey'));
 
-        // $endpoint_secret = 'whsec_VsnjOx8yRSMs7cjwFxHfw0kaj3NASwKU';
+        $endpoint_secret = 'whsec_VsnjOx8yRSMs7cjwFxHfw0kaj3NASwKU';
 
         $payload = @file_get_contents('php://input');
         $event = null;
         
         try {
+
           $event = \Stripe\Event::constructFrom(
             json_decode($payload, true)
           );
+
         } catch(\UnexpectedValueException $e) {
-          // Invalid payload
-          echo '⚠️  Webhook error while parsing basic request.';
+
           http_response_code(400);
           exit();
         }  
         
-        // if ($endpoint_secret) {
-        //   // Only verify the event if there is an endpoint secret defined
-        //   // Otherwise use the basic decoded event
-        //   $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-        //   try {
-        //     $event = \Stripe\Webhook::constructEvent(
-        //       $payload, $sig_header, $endpoint_secret
-        //     );
-        //   } catch(\Stripe\Exception\SignatureVerificationException $e) {
-        //     // Invalid signature
-        //     echo '⚠️  Webhook error while validating signature.';
-        //     http_response_code(400);
-        //     exit();
-        //   }
-        // }
+        if ($endpoint_secret) {
+          // Only verify the event if there is an endpoint secret defined
+          // Otherwise use the basic decoded event
+          $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+          try {
+            $event = \Stripe\Webhook::constructEvent(
+              $payload, $sig_header, $endpoint_secret
+            );
+          } catch(\Stripe\Exception\SignatureVerificationException $e) {
+            // Invalid signature
+            echo '⚠️  Webhook error while validating signature.';
+            http_response_code(400);
+            exit();
+          }
+        }
 
-        // Handle the event
+        // HANDLE THE EVENT
+        // https://stripe.com/docs/api/events/types
         switch ($event->type) {
 
           case 'customer.subscription.updated':
-
             $subscription = $event->data->object;
 
+            // UPDATE STRIPE SUBSCRIPTION FOR USER X 
             kirby()->site()->updateStripeSubscriptionWebhook($subscription);  
 
             break;
 
           case 'customer.subscription.deleted':
-
             $subscription = $event->data->object;
-            kirby()->site()->cancelStripeSubscriptionWebhook($subscription); 
 
+            // RESET KIRBY USER SUBSCRIPTION INFO
+            kirby()->site()->cancelStripeSubscriptionWebhook($subscription); 
 
             break;
 
           case 'customer.updated':
-
             $customer = $event->data->object;
+
+            // UPDATE KIRBY USER EMAIL
             kirby()->site()->updateStripeEmailWebhook($customer); 
 
             break;
 
-          
-
           default:
-            // Unexpected event type
-            echo 'Received unknown event type';
+
+            throw new Exception('Received unknown stripe event type!');
         }
 
-        // http_response_code(200);
-
-        return '<html><body>Success!</body></html>';
+        http_response_code(200);
+        return '<html><body>✔️ Success!</body></html>';
       },
+      // ENSURE ONLY POST REQUESTS ARE CAPTURED
       'method' => 'POST'
     ],
   ];
