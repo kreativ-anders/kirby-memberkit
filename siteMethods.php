c75bb2516fed2b3a27d72c8516fd4d027f687b90
Manuel Steinberg <github@manuel-steinberg.de>
2021-03-05 11:16:39 +0100

    add try catch blocks
    throw exceptions when something went wrong

--- siteMethods.php
+++ siteMethods.php
@@ -13,50 +13,78 @@ return [
 
     $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
 
-    $customer = $stripe->customers->retrieve(
-      $subscription->customer,
-      []
-    );
+    try {
 
+      // GET STRIPE CUSTOMER 
+      $customer = $stripe->customers->retrieve(
+        $subscription->customer,
+        []
+      );
+
+    } catch (Exception $e) {
+      
+      // LOG ERROR SOMEWHERE
+      throw new Exception('Could not retrieve stripe customer!');
+    }    
+
+    // DETERMINE TIER NAME BY STRIPE PRICE ID
     $price = $subscription->items['data'][0]->price->id;
     $priceIndex = array_search($price, array_column(option('kreativ-anders.memberkit.tiers'), 'price'), false);
-
     $tier = option('kreativ-anders.memberkit.tiers')[$priceIndex]['name'];
 
     $kirby = kirby();
     $kirby->impersonate('kirby');
 
-    kirby()->user($customer->email)->update([
-      'stripe_subscription' => $subscription->id,
-      'stripe_status' => $subscription->status,
-      'tier' => $tier
-    ]);
+    try {
 
-    $kirby->impersonate();    
+      // UPDATE KIRBY USER SUBSCRIPTION INFORMATION
+      kirby()->user($customer->email)->update([
+        'stripe_subscription' => $subscription->id,
+        'stripe_status' => $subscription->status,
+        'tier' => $tier
+      ]);
+
+    } catch (Exception $e) {
+      
+      // LOG ERROR SOMEWHERE
+      throw new Exception('Could not update kirby user!');
+    }
+    
+    $kirby->impersonate();        
   },
   // CANCEL STRIPE SUBSCRIPTION VIA WEBHOOK
   'cancelStripeSubscriptionWebhook' => function ($subscription) {
 
     $stripe = new \Stripe\StripeClient(option('kreativ-anders.memberkit.secretKey'));
 
+    // GET STRIPE CUSTOMER
     $customer = $stripe->customers->retrieve(
       $subscription->customer,
       []
     );
 
+    // DETERMINE TIER NAME BY STRIPE PRICE ID
     $price = $subscription->items['data'][0]->price->id;
     $priceIndex = array_search($price, array_column(option('kreativ-anders.memberkit.tiers'), 'price'), false);
-
     $tier = option('kreativ-anders.memberkit.tiers')[$priceIndex]['name'];
 
     $kirby = kirby();
     $kirby->impersonate('kirby');
 
-    kirby()->user($customer->email)->update([
-      'stripe_subscription' => null,
-      'stripe_status' => null,
-      'tier' => option('kreativ-anders.memberkit.tiers')[0]['name']
-    ]);
+    try {
+      
+      // RESET KIRBY USER SUBSCRIPTION INFORMATION
+      kirby()->user($customer->email)->update([
+        'stripe_subscription' => null,
+        'stripe_status' => null,
+        'tier' => option('kreativ-anders.memberkit.tiers')[0]['name']
+      ]);
+
+    } catch (Exception $e) {
+      
+      // LOG ERROR SOMEWHERE
+      throw new Exception('Could not reset kirby user!');
+    } 
 
     $kirby->impersonate();    
   },
@@ -66,7 +94,16 @@ return [
     $kirby = kirby();
     $kirby->impersonate('kirby');
 
-    $kirby->users()->findBy('stripe_customer', $customer->id)->changeEmail($customer->email);
+    try {
+      
+      // UPDATE KIRBY USER EMAIL
+      $kirby->users()->findBy('stripe_customer', $customer->id)->changeEmail($customer->email);
+
+    } catch (Exception $e) {
+      
+      // LOG ERROR SOMEWHERE
+      throw new Exception('Could not change kirby user email!');
+    } 
 
     $kirby->impersonate();    
   }
